c963c913a240600cef12d8a99cd31f3dffa4eb64
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-03-21 22:02:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question no 2
+let person_name = "Eric";
+console.log(`Hello ${person_name}, would you like to learn some Python today?`);
+// Question no 3 use previous person name
+console.log(person_name.toUpperCase());
+console.log(person_name.toLowerCase());
+console.log(person_name.to());
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Question no 2
+
+let person_name:string = "Eric";
+console.log(`Hello ${person_name}, would you like to learn some Python today?`);
+
+// Question no 3 use previous person name
+console.log(person_name.toUpperCase());
+console.log(person_name.toLowerCase());
+console.log(person_name.to());
